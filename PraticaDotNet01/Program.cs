a896ff921f6836c6818e6684b6ae46504016259d
amarildolucena <amarildolucena@gmail.com>
2022-01-20 22:23:14 -0300

    Primeira Aula Pratica do GFT Start3 #NET

---
+++ PraticaDotNet01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PraticaDotNet01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+
+            for (int i=0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem-vindo ao Bootcamp GFT Start #Net {i}!");
+            }
+        }
+    }
+}
